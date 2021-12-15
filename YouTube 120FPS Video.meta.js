9284fbc791608ab5fb7c3e5de13a2b2a2aedcd35
DiopinYT <85357474+MrCoder228re@users.noreply.github.com>
2021-12-15 17:09:42 +0500

    Create YouTube 120FPS Video.meta.js

---
+++ YouTube 120FPS Video.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         YouTube 120FPS Video
+// @namespace    http://api.diopinyt.xyz/yt120fps
+// @version      0.2
+// @updateURL    https://github.com/MrCoder228re/yt120fps/raw/main/YouTube%20120FPS%20Video.meta.js
+// @downloadURL  https://github.com/MrCoder228re/yt120fps/raw/main/YouTube%20120FPS%20Video.user.js
+// @description  120FPS video on YouTube!
+// @author       DiopinYT
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @grant        none
+// ==/UserScript==
